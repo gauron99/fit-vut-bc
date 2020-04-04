67ba316e590e4532fd6926850b6cdcfd005f26af
David Fridrich <fridrich.david19@gmail.com>
2020-04-04 21:14:39 +0200

    initialized interpret

---
+++ interpret.py
@@ -0,0 +1,7 @@
+#########################################
+# Autor : David Fridrich                #
+# Tema  : IPP2020 interpret             #
+# Posledni zmena : 04.04.2020           #
+#########################################
+
+print ('Hello')
