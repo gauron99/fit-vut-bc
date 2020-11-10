62506ac989fca5450a33e7ef6500ec39cdf64a74
l0uda <louda@Arch13.localdomain>
2020-11-10 22:11:17 +0100

    A
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    A
    D
    D
    D
    C
    C
    C
    C

---
+++ ifj20.c
@@ -0,0 +1,24 @@
+//
+// Created by louda on 11/10/20.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "ifj20.h"
+
+int rdBody(){
+    return 0;
+}
+
+int main(){
+
+    CHECK(rdBody())
+
+    /*while (token->type != T_EOF)
+    {
+        CHECK(Get_Token(wrap.tkn))
+    }*/
+
+    printf("pohoda\n");
+    return 0;
+}
---
+++ ifj20.h
@@ -0,0 +1,11 @@
+//
+// Created by louda on 11/10/20.
+//
+
+#ifndef IFJ_IFJ20_H
+#define IFJ_IFJ20_H
+
+#define CHECK(arg) int ret = rdBody(); if (ret) return ret;
+
+
+#endif //IFJ_IFJ20_H
