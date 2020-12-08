9c867c8f3c0318647dc190f3152491faf4a88650
sabinathe <41567711+sabgul@users.noreply.github.com>
2020-12-08 10:30:45 +0100

    Add lexPASS

---
+++ tests/tests/lexPASS/test03.go
@@ -0,0 +1,5 @@
+package main
+
+func main () {
+	// some /* minor */ comments
+}
---
+++ tests/tests/lexPASS/test28.go
@@ -0,0 +1,7 @@
+package main
+
+func main () {
+	if(0o101001_1_0_001 >= 0XFFFF_FFFF_1111_ffff) {
+		print("ratata")
+	}
+}
