e99bc10357c6121e7755689fd019ddb7444cf901
David Fridrich <fridrich.david19@gmail.com>
2021-11-26 12:45:56 +0100

    api fe + be mid boy init

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 
+// setup using body-parser (is integrated in express itself?) for POST requests
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
 // Database setup
 // const mysql = require('mysql');
 // const db = mysql.createPool({
@@ -12,32 +16,38 @@ const PORT = 8000;
 //   // database: 'the-db',
 // });
 
-// add middleware
+// add middleware -- static files can now be used from frontend
 app.use(express.static("client/build"));
 
 
+app.get("/api*",(req,res) => {
+  let O = JSON.stringify(req.query); //this works
+  let m = req.query.from;
+  let n = req.query.to;
+  let o = req.query.date;
+  let p = req.query.time;
+  console.log(m)
+
+  console.log(req.params)
+  console.log(req.params[0])
+  res.json({m: "api call bakc boi|" + O + "|"+ m + n + o +p})
+});
 
 // https://stackoverflow.com/questions/54282344/how-does-express-and-react-routes-work-on-initial-get-request-from-browser
 // /api is the connector to the backend, exclude anything that matches "/api"
-app.get(/^\/(?!api).*/mg, (req, res, next) => {
+
+//TODO might move this under /api handler because priority exists and the handler
+//above WILL handle FIRST. No Next() will be called therefore the second handler
+//wont do anything... test if works
+
+// app.get(/^\/(?!api).*/mg, (req, res, next) => {
+//   res.send("index.html")
+//   next()
+// });
+app.get("*", (req, res, next) => {
   res.send("index.html")
   next()
 });
-
-app.get("/api*",(req,res) => {
-  var params = req.params[0];
-  console.log(req.params)
-  console.log(params)
-});
-
-
-app.get("/login",(req,res) => {
-  res.send("login scrn");
-})
-
-app.get("/admin",(req,res) => {
-  res.send("overlord page");
-})
 
 app.listen(PORT, () => {
   console.log("listening on port %s",PORT);
